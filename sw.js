dc51abd3c2b65055776b5b8db4ea1c2179a570df
[mohadese sakhaie] <[m.sakhaie599@gmail.com]>
2021-04-09 02:19:14 +0430

    make button work with adding function on it

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//variable
+//variable for set time without functions
 let hour = 00;
 let min = 00;
 let sec = 00;
@@ -7,23 +7,22 @@ let displaySec = 00;
 let displayMin = 00;
 let displayHour = 00;
 
-// var appendMin = document.getElementById("min");
-// var appendSec = document.getElementById("sec");
 
-// var start = document.getElementById("start");
-// var stop = document.getElementById("stop");
-// var reset = document.getElementById("reset");
+// var for future function in buttons
+ var startButton = document.getElementById("start");
+ var stopButton = document.getElementById("stop");
+ var resetButton = document.getElementById("reset");
 
 
 //functions
 
 const startTimer = () => {
     sec += 1;
-    if (sec / 60 === 1) {
+    if (sec % 60 === 0) {
         sec = 00;
         min += 1;
 
-        if (min / 60 === 1) {
+        if (min % 60 === 0) {
             min = 00;
             hour++;
         }
@@ -41,10 +40,22 @@ const startTimer = () => {
         displayMin = min;
     }
 
-
     document.getElementById("timers").innerHTML = displayHour + ":" + displayMin + ":" + displaySec;
 }
 
-window.setInterval(startTimer, 1000);
+//buttons function
 
+startButton.onclick = function () {
+    interval = setInterval(startTimer,1000);
+}
+stopButton.onclick = function () {
+    clearInterval(interval);
+}
+resetButton.onclick = function () {
+    clearInterval(interval);
+    hour = "00";
+    min = "00";
+    sec = "00";
+    document.getElementById("timers").innerHTML = "00:00:00";
+};
 
